f6cf190372983ac3f394b87da4ab53c483c72213
Hanseuly <mir9559@gmail.com>
2016-10-18 19:26:35 +0900

    first page:index.html / add list.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  root 'home#main'
+  # root 'home#main'
+  root 'home#index'
   match "/:controller(/:action(/:id))", :via => [:get, :post]
   post "/post/create", to: "post#create"
-  
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
