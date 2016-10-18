5afc9deb7e0e811829f8f7d6cf6c057d8df17fb8
Hanseuly <mir9559@gmail.com>
2016-10-18 22:36:45 +0900

    list page -ing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,37 @@
 //= require_tree .
 
 
+///// Floating div ///////////////
+// $(document).ready(function(){
+//     $('.a').each(function(){ animateDiv($(this)); });
+// });
+//
+// function makeNewPosition(){
+//     var h = $(window).height() - 50;
+//     var w = $(window).width() - 50;
+//     var nh = Math.floor(Math.random() * h);
+//     var nw = Math.floor(Math.random() * w);
+//     return [nh,nw];
+// }
+//
+// function animateDiv(ele){
+//     var newq = makeNewPosition();
+//     var oldq = ele.offset();
+//     var speed = calcSpeed([oldq.top, oldq.left], newq);
+//     ele.animate({ top: newq[0], left: newq[1] }, speed, function(){
+//       animateDiv(ele);
+//     });
+// };
+//
+// function calcSpeed(prev, next) {
+//     var x = Math.abs(prev[1] - next[1]);
+//     var y = Math.abs(prev[0] - next[0]);
+//     var greatest = x > y ? x : y;
+//     var speedModifier = 0.05;
+//     var speed = Math.ceil(greatest/speedModifier);
+//     return speed;
+// }
+
 
 
 //// Draged TEXT /////////////
@@ -51,7 +82,7 @@ document.onmouseup = function() {
   document.getElementById("myModalLabel2").innerHTML = selectedText;
   document.getElementById("myModalLabel3").innerHTML = selectedText;
   document.getElementById("myModalLabel4").innerHTML = selectedText;
-  
+
   document.getElementById("locationText1").innerHTML = selectedText;
   document.getElementById("locationText2").innerHTML = selectedText;
   document.getElementById("locationText3").innerHTML = selectedText;
